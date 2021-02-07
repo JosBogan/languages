bfad534dbe5173437113f664853c14293bc57669
Jos Bogan <Josbogan@gmail.com>
2021-02-07 17:49:57 +0000

    fix

---
+++ frontend/build/precache-manifest.a4b5974fe2fdd680d9067aedb9cf9c08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b243486f7ed1e20871493dc766c98d5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1169e3c1d0152b5e69d",
+    "url": "/static/css/main.24a643b6.chunk.css"
+  },
+  {
+    "revision": "47550f582d3fa7b9f9c1",
+    "url": "/static/js/2.e77c88d4.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.e77c88d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1169e3c1d0152b5e69d",
+    "url": "/static/js/main.414d1f4b.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  },
+  {
+    "revision": "7c444a7bf5a044696db423a22e5ff760",
+    "url": "/static/media/hero_copy.7c444a7b.svg"
+  },
+  {
+    "revision": "38072b066d9d14409d19f24b3e5310f4",
+    "url": "/static/media/japan_hero.38072b06.jpg"
+  }
+]);
