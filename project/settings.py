f7561d520bfd4f0bf3dc7f5f79affa95ad0476fb
Jos Bogan <Josbogan@gmail.com>
2021-02-07 17:33:12 +0000

    staticfix

--- project/settings.py
+++ project/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
@@ -169,7 +169,7 @@ AUTH_USER_MODEL = 'jwt_auth.User'
 CORS_ORIGIN_ALLOW_ALL = True
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend', "build", "static"), 
+    os.path.join(BASE_DIR, 'frontend', 'build', 'static'), 
 )
 
 django_heroku.settings(locals())
